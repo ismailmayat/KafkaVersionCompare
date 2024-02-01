200adab5c63c84fa82fdffc188d8c779e05dc72a
Ismail Mayat <imayat@confluent.io>
2024-02-01 07:40:18 +0000

    updated index page and split out to pages kafka and cp compare

---
+++ KafkaVersionCompare/Pages/KafkaCompare.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KafkaVersionCompare.Pages;
+
+public class KafkaCompare : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public KafkaCompare(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
